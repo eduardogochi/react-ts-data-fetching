22e6c42a8625a9222dbfa6635dc396d701441c96
Eduardo Gochi <eduardo.gochi@gmail.com>
2024-01-29 15:52:04 -0600

    Created component to fetch posts from jsonplaceholder

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ReactNode, useEffect, useState } from 'react'
+
+import BlogPosts, { BlogPost } from './components/BlogPosts'
+import { get } from './util/http'
+import fetchImg from './assets/data-fetching.png'
+
+type RawDataBlogPost = {
+  id: number
+  title: string
+  body: string
+  userId: number
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [fetchedPosts, setFetchedPosts] = useState<BlogPost[]>()
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  useEffect(() => {
+    async function fetchPosts() {
+      const data = (await get('https://jsonplaceholder.typicode.com/posts')) as RawDataBlogPost[]
+
+      const blogPosts: BlogPost[] = data.map((rawPost) => {
+        return {
+          id: rawPost.id,
+          title: rawPost.title,
+          text: rawPost.body
+        }
+      })
+
+      setFetchedPosts(blogPosts)
+    }
+
+    fetchPosts()
+  }, [])
+
+  let content: ReactNode
+
+  if (fetchedPosts) {
+    content = <BlogPosts posts={fetchedPosts} />
+  }
+
+  return <main>
+    <img src={fetchImg} alt='img' />
+    {content}
+  </main>
 }
 
 export default App
