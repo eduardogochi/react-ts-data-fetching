37366f508b685acb3f45fde7fc371008fc2964a1
Eduardo Gochi <eduardo.gochi@gmail.com>
2024-01-29 17:07:41 -0600

    Added zod library to validate data and handling of errors

--- src/App.tsx
+++ src/App.tsx
@@ -3,30 +3,47 @@ import { ReactNode, useEffect, useState } from 'react'
 import BlogPosts, { BlogPost } from './components/BlogPosts'
 import { get } from './util/http'
 import fetchImg from './assets/data-fetching.png'
+import { z } from 'zod'
+import ErrorMessage from './components/ErrorMessage'
 
-type RawDataBlogPost = {
-  id: number
-  title: string
-  body: string
-  userId: number
-}
+const rawDataBlogPostSchema = z.object({
+  id: z.number(),
+  title: z.string(),
+  body: z.string(),
+  userId: z.number()
+})
+
+const expectedResponseDataSchema = z.array(rawDataBlogPostSchema)
 
 function App() {
   const [fetchedPosts, setFetchedPosts] = useState<BlogPost[]>()
+  const [isFetching, setIsFetching] = useState(true)
+  const [error, setError] = useState<string>()
 
   useEffect(() => {
     async function fetchPosts() {
-      const data = (await get('https://jsonplaceholder.typicode.com/posts')) as RawDataBlogPost[]
+      setIsFetching(true)
+      try {
 
-      const blogPosts: BlogPost[] = data.map((rawPost) => {
-        return {
-          id: rawPost.id,
-          title: rawPost.title,
-          text: rawPost.body
+        const data = (await get('https://jsonplaceholder.typicode.com/posts', z.array(rawDataBlogPostSchema)))
+        const parsedData = expectedResponseDataSchema.parse(data)
+
+        const blogPosts: BlogPost[] = parsedData.map((rawPost) => {
+          return {
+            id: rawPost.id,
+            title: rawPost.title,
+            text: rawPost.body
+          }
+        })
+        setIsFetching(false)
+        setFetchedPosts(blogPosts)
+
+      } catch (err) {
+        if (err instanceof Error) {
+          setError(err.message)
         }
-      })
 
-      setFetchedPosts(blogPosts)
+      }
     }
 
     fetchPosts()
@@ -34,9 +51,16 @@ function App() {
 
   let content: ReactNode
 
+  if (error) {
+    content = <ErrorMessage text={error} />
+  } else if (isFetching) {
+    content = <p id='loading-fallback'>Fetching posts...</p>
+  }
+
   if (fetchedPosts) {
     content = <BlogPosts posts={fetchedPosts} />
   }
+
 
   return <main>
     <img src={fetchImg} alt='img' />
--- src/util/http.ts
+++ src/util/http.ts
@@ -1,4 +1,6 @@
-export async function get(url: string) {
+import { z } from 'zod'
+
+export async function get(url: string, zodSchema: z.ZodType<T>) {
   const response = await fetch(url)
 
   if (!response.ok) {
@@ -6,5 +8,10 @@ export async function get(url: string) {
   }
 
   const data = (await response.json()) as unknown
-  return data
+
+  try {
+    return zodSchema.parse(data)
+  } catch (err) {
+    throw new Error('Invalid data received from server')
+  }
 }
